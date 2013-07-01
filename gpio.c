0f0749867d220fda16a184273044d59a1922764d
Paul Barker <paul@paulbarker.me.uk>
2013-07-01 11:31:45 +0100

    gpio: Update formatting to match header
    Signed-off-by: Paul Barker <paul@paulbarker.me.uk>

--- gpio.c
+++ gpio.c
@@ -1,28 +1,32 @@
-/*******************************************************************************
-        gpio.c: GPIO interface for ads1672 driver.
+/*
+ * Copyright (C) 2011-2013 Paul Barker, Loughborough University
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
 
-        Copyright (C) 2011-2013 Paul Barker, Loughborough University
-
-	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program; if not, write to the Free Software
-	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-*******************************************************************************/
+/*
+ * gpio.c
+ * GPIO interface for ads1672 driver.
+ */
 
 #include <linux/gpio.h>
 #include <plat/mux.h>
 
 #include "gpio.h"
 
+/* GPIO pin numbers. */
 #define ADS1672_GPIO_START      138
 #define ADS1672_GPIO_SELECT     139
 
